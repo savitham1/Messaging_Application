4916f408e0e7453d70f734552a1e1675313baeb2
Savitha Munirajaiah <savitham1@icloud.com>
2022-02-24 07:35:42 -0500

    Sign Up/ Sign In form with User Authentication

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -4,17 +4,44 @@ import axios from 'axios';
 
 import signInImage from '../assets/signup.jpeg';
 
-const Auth = () => {
-    const [ isSignup, setIsSignUp ] = useState(true);
+// Create an object with initial state of the form (empty).
+const initialState = {
+    fullName : '',
+    userName : '',
+    password : '',
+    avatarURL : '',
+    confirmPassword : '',
+    phoneNumber : '',
+}
 
-    const handleChange = () => {}
+const Auth = () => {
+    const [ isSignup, setIsSignUp ] = useState(true); // Maintain the state of user login. user is logged in or not. 
+    const [ form, setForm ] = useState(initialState);
+
+    const handleChange = (event) => {
+        setForm({... form, [event.target.name] : event.target.value }); // wrap the key in square brackets (it is extracted from input tag's name
+        // field) to update its value
+       
+    } // gets all the input from the form provided by the user.
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); // to avaoid reach from reloading the page. we want to maintain the state until the form is submitted.
+
+        console.log(form);
+    }
+
+    const switchMode = () => {
+        // setIsSignUp(!isSignup) ----NOT BEST PRACTICE
+        // Correct way to change the state depending on its previours state
+        setIsSignUp((prevIsSignUp) => !prevIsSignUp)  
+    }
     return (
         <div className="auth__form-container">
             <div className="auth__form-container_fields">
                 <div className="auth__form-container_fields-content">
                     {/* Check if we are on the Signup form or not */}
                     <p>{isSignup ? 'Sign Up' : 'Sign In'}</p>
-                    <form onSubmit={() => {}}>
+                    <form onSubmit={handleSubmit}>
                         {/* if SignUp is true then execute the div */}
                         {isSignup && (
                             <div className="auth__form-container_fields-content_input">
@@ -62,8 +89,46 @@ const Auth = () => {
                                 />
                             </div>
                         )}
+                        <div className="auth__form-container_fields-content_input">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                onChange={handleChange}
+                                required // This is one of those * field!!!
+                            />
+                        </div>
+                        {isSignup && (
+                            <div className="auth__form-container_fields-content_input">
+                                <label htmlFor="confirmPassword">Confirm Password</label>
+                                <input
+                                    name="confirmPassword"
+                                    type="password"
+                                    placeholder="Confirm Password"
+                                    onChange={handleChange}
+                                    required // This is one of those * field!!!
+                                />
+                            </div>
+                        )}
+                        <div className="auth__form-container_fields-content_button">
+                            <button>{isSignup ? 'Sign In' : 'Sign Up'}</button>
+                        </div>
                     </form>
+
+                    {/* To Switch between SignUp and SignIn page if user already has an account */}
+                    <div className="auth__form-container_fields-account">
+                        <p>
+                            {isSignup ? "Already have an account?" : "Don't have an account?"}
+                            <span onClick={switchMode}>
+                                {isSignup ? 'Sign In' : 'Sign Up'}
+                            </span>
+                        </p>
+                    </div>
                 </div>
+            </div>
+            <div className="auth__form-container_image">
+                <img src={signInImage} alt="sign in" />
             </div>
         </div>
     )
